189e0dc1aa530ca248a017e53cbeef4df7f9170c
Stephen Wright <sjw948@gmail.com>
2017-03-28 10:16:48 +0100

    recipe book base files
    components for recipes, recipe list, recipe items, recipe details, shopping list and shopping list add

---
+++ recipe-book/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent, RecipeListComponent, RecipeItemComponent, RecipeDetailComponent } from './recipes';
+import { ShoppingListComponent, ShoppingListAddComponent } from "./shopping-list";
+
+@Component({
+  selector: 'rb-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+
+}
---
+++ recipe-book/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent, RecipeListComponent, RecipeItemComponent, RecipeDetailComponent } from "./recipes";
+import { ShoppingListComponent, ShoppingListAddComponent } from './shopping-list/';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListAddComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
