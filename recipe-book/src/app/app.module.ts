0b4b3b60b47daaf5e5d324be51025d71a2f371b8
Stephen Wright <sjw948@gmail.com>
2017-03-31 12:42:34 +0100

    created a core module to reduce app module clutter

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -5,23 +5,21 @@ import { HttpModule } from '@angular/http';
 import { Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { DropdownDirective } from './dropdown.directive';
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { RecipeService } from "./recipes/recipe.service";
 import { routing } from "./app.routes";
-import { HomeComponent } from "./home.component";
+import { CoreModule } from "./core.module"
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     routing,
+    CoreModule,
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent],
